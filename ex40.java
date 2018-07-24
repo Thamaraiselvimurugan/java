d55453274c120d0f6c6243d1c09c4a99331c6ca1
Thamaraiselvimurugan <40590355+Thamaraiselvimurugan@users.noreply.github.com>
2018-07-24 22:49:55 +0530

    Add files via upload

---
+++ ex40.java
@@ -0,0 +1,17 @@
+import java.nio.charset.Charset;
+
+public class thams40 {
+
+  public static void main(String[] args) {
+
+	System.out.println("List of available character sets: ");  
+
+    for (String str : Charset.availableCharsets().keySet()) {
+
+      System.out.println(str);
+
+    }
+
+  }
+
+}
---
+++ ex41.java
@@ -0,0 +1,13 @@
+public class thams41 {
+
+
+
+	public static void main(String[] String) {
+
+		int chr = 'Z';
+
+		System.out.println("The ASCII value of Z is :"+chr);
+
+	}
+
+}

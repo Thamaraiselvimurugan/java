873aee6632ac059ebf94ccf7cd2653edc59fc806
Thamaraiselvimurugan <40590355+Thamaraiselvimurugan@users.noreply.github.com>
2018-07-25 01:02:51 +0530

    Add files via upload

---
+++ ex84.java
@@ -0,0 +1,29 @@
+import java.util.*; 
+
+import java.io.*; 
+
+ public class thams84 {
+
+ public static void main(String[] args)
+
+ {
+
+   String string1 = "thamaraiselvi";
+
+    int slength = 3;
+
+    if (slength > string1.length()) {
+
+      slength = string1.length();
+
+    }
+
+
+
+    String subpart = string1.substring(string1.length()-3);
+
+    System.out.println(subpart + string1 + subpart);
+
+  }
+
+}
---
+++ ex85.java
@@ -0,0 +1,17 @@
+import java.util.*; 
+
+import java.io.*; 
+
+ public class thams85 {
+
+ public static void main(String[] args)
+
+ {
+
+   String string1 = "Hello This is thamsthamarai";
+
+    System.out.println(string1.startsWith("Hello"));
+
+  }
+
+}
